8abde9598b0d8169c245f77b2d5cb85362316f0e
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-02-16 15:34:15 +0000

    update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,12 @@ int main(void)
 	int b;
 	long int c;
 	long long int d;
-	float d;
+	float e;
 
 	printf("Size of a char: is %u\n", (unsigned int)sizeof(a));
 	printf("Size of a int: is %u\n", (unsigned int)sizeof(b));
 	printf("Size of a long int: is %u\n", (unsigned int)sizeof(c));
-	printf("Size of a float: is %u\n", (unsigned int)sizeof(d));
+	printf("Size of a long long int: is %u\n", (unsigned int)sizeof(d));
+	printf("Size of a float: is %u\n", (unsigned int)sizeof(e));
 	return (0);
 }
