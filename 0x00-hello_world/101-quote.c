0c179e38d31687ebccb9c685367cea004ed332c1
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-02-16 16:36:36 +0000

    update file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,8 @@
 
 int main(void)
 {
-	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015
--10-19\n";
+	char message[] =
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	syscall(SYS_write, 1, message, sizeof(message));
 	return (1);
 }
