bd831feac16be1f4895518ba57e9ac1a88f11bb7
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-02-17 14:56:37 +0000

    add file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+*main - Prints all single digits with , and space followed by new line
+*
+*Return: returns 0
+*/
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+		if (num != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
