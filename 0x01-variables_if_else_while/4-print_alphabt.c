f9229fbf481a3eb3ab345e7ec2c5fa4dde84d16f
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-02-17 13:45:50 +0000

    add file

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,27 @@
-#include
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  *main - main block
+  *Return: 0
+  */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter == 'q' || letter == 'e')
+		{
+			continue;
+		}
+		else
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+
+	return (0);
+}
