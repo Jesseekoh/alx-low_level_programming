a54c8b40a5f2141f60d63ffa994791cd7d655efa
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-02-21 18:06:02 +0000

    update file

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 
 /**
   * add - adds two ints and returns their sum
-  * @a - int to be added to b
-  * @b - int to be added to a
+  * @a: int to be added to b
+  * @b: int to be added to a
   * Return: the sum of a and b
   */
 
