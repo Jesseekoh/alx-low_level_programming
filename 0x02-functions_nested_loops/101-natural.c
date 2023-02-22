600a54020a103e06c410620c414f0b1b0bc8e225
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-02-22 01:52:05 +0000

    add file 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * main - prints the sum of all multiples of 3 less than 1024
+  * Return: 0
+  */
+
+int main(void)
+{
+	int i;
+	int sum;
+	int limit = 1024;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum+= i;
+		else
+			continue;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
