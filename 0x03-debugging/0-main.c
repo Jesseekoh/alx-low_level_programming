bbddc0801e4f4cdcec10cf5e515b7caa119df260
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-02-22 16:08:16 +0000

    add files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * main - tests the positive_or_negative function
+  * Return: 0
+  */
+
+int main(void)
+{
+	positive_or_negative(0);
+
+	return(0);
+}
