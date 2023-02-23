28e193e090b62db40114e2a32119ee2370adf9eb
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-02-23 16:23:25 +0000

    add file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * print_line - prints the "-" character a number of times
+  * @n: number of times to print "_"
+  * Return: no return
+  */
+
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		while (n >= 0)
+		{
+			_putchar('_');
+			n--;
+		}
+	}
+}
