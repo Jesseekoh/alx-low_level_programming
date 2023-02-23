631dd7c49cd89ca7bd9e0934cf3c5ae43997954a
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-02-23 16:59:18 +0000

    add file

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * print_diagonal - prints forward slashes
+  * @n: number of ot spaces before slash
+  * Return: no return
+  */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n < 1)
+		_putchar('\n');
+	for (i = o; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
