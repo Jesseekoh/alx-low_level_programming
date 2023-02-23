4a7529a6687e1349026b8e94f221e2930f88acef
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-02-23 17:50:38 +0000

    add file

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+  * main - prints number
+  * Return: void
+  */
+
+void main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ")
+		}
+		else if ((i % 5 == 0) && (i % 3 == 0))
+		{
+			printf("FIzzBuzz ");
+		}
+		else
+			printf("%d ", i);
+
+		printf("\n");
+
+	}
+}
