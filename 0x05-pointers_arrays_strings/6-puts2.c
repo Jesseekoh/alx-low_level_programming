72c261b31d30405a82142f66fa55a8bfd65c4d6d
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-02-27 21:47:50 +0000

    update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,33 +9,14 @@
 void puts2(char *str)
 {
 	int i = 0;
-	int lengthOfStr = _strlen(str);
 
-	while (i < lengthOfStr - 1)
+	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
-		i += 2;
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
 
 
-/**
-  * _strlen - returns the length of a string
-  * @s: string to be checked
-  * Return: length of string
-  */
 
-int _strlen(char *s)
-{
-	int i = 0;
-	int length = 0;
-
-	while (s[i] != '\0')
-	{
-		length++;
-		i++;
-	}
-
-	return (length);
-}
