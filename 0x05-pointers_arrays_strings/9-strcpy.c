3c32e8f0fa7c583e72da837b3fe630620f7eeae9
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-02-28 01:58:11 +0000

    add 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * _strcpy - copies a string to a pointed variable
+  * @dest: target pointed variable
+  * @src: string to copy
+  */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[a] != '\0')
+	{
+		dest[a] = src[a];
+		i++;
+	}
+
+	dest[a] = '\0';
+
+	return (dest);
+}
