79693e3c3c608b0a9631553396ec311aa8000f5e
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-03-03 02:36:45 +0000

    add 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * _strcat(char *dest, char *src)
+  * @dest: First string
+  * @src: string to be appended to dest
+  * Return: the concatenated string
+  */
+
+char *_strcat(char *dest, char *src)
+{
+	int len;
+	int i;
+
+
+	for (len = 0; dest[len] != '\0'; len++)
+	{
+
+	}
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[l + i] = src[i];
+
+	}
+
+	return (dest);
+}
