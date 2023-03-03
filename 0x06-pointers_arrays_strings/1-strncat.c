8f96dfd85b7b6b167f4ebe0f4d4a77601ecbfde4
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-03-03 02:53:42 +0000

    update file

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,16 +14,13 @@ char *_strncat(char *dest, char *src, int n)
 
 	if (n <= 0)
 		return (dest);
-
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
-
 	for (j = 0; j < n && src[j] != '\0'; j++)
 	{
 		dest[i + j] = src[j];
 	}
-
 	dest[i++] = '\0';
 	return (dest);
 }
