85099a6817ad33040254b1e60afefdc7e479a5ac
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-03-03 02:52:15 +0000

    add 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+  * _strncat - concats string
+  * @dest: string
+  * @src: string
+  * @n: number of bytes
+  * Return: string
+  */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		return (dest);
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
+		dest[i + j] = src[j];
+	}
+
+	dest[i++] = '\0';
+	return (dest);
+}
+
+
