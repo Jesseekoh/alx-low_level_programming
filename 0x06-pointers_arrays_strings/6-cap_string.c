c4fd0b6ad428963c9e12d60f5ae84d6cd55153f1
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-03-03 05:08:51 +0000

    update file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,12 +13,12 @@ char *cap_string(char *str)
 		str[0] = str[0] - 32;
 	while (str[i] != '\0')
 	{
-		if (str[i -1] == ' ' ||
+		if (str[i - 1] == ' ' ||
 			str[i - 1] == '\n'||
 			str[i - 1] == '\t' ||
-			str[i -1] == ';' ||
-			str[i -1] == '.' ||
-			str[i -1] == ',' ||
+			str[i - 1] == ';' ||
+			str[i - 1] == '.' ||
+			str[i - 1] == ',' ||
 			str[i - 1] == '?' ||
 			str[i - 1] == '"' ||
 			str[i - 1] == '(' ||
