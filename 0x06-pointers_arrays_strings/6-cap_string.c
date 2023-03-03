e20ce9eaca77123c1775fda45a3f85e8c193c11f
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-03-03 07:16:32 +0000

    update file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,24 +11,19 @@ char *cap_string(char *str)
 
 	if (str[0] >= 'a' && str[0] <= 'z')
 		str[0] = str[0] - 32;
+
 	while (str[i] != '\0')
 	{
-		if (str[i - 1] == ' ' ||
-			str[index - 1] == '\t' ||
-			str[index - 1] == '\n' ||
-			str[index - 1] == ',' ||
-			str[index - 1] == ';' ||
-			str[index - 1] == '.' ||
-			str[index - 1] == '!' ||
-			str[index - 1] == '?' ||
-			str[index - 1] == '"' ||
-			str[index - 1] == '(' ||
-			str[index - 1] == ')' ||
-			str[index - 1] == '{' ||
-			str[index - 1] == '}')
+		if ((str[i - 1] == ' ' || str[i - 1] == '\t'
+		     || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';'
+		     || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?'
+		     || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')'
+		     || str[i - 1] == '{' || str[i - 1] == '}')
+		     && (str[i] >= 'a' && str[i] <= 'z'))
 		{
-			str[i] -= 32;
+			str[i] = str[i] - 32;
 		}
+		i++;
 	}
 	return (str);
 }
