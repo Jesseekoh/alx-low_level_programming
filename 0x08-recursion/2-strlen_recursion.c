4b100d7177513eedabc488b92655f468e5d497e8
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-03-07 16:19:02 +0000

    add file

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * _strlen_recursion - returns the length of a string
+  * @s: string
+  * Return: length of s
+  */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		_strlen_recursion(s + 1);
+		i++;
+	}
+
+	return (i);
+
+}
