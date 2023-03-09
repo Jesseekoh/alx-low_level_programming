dd513df794f443fc68382f4ecd5101737aabe30f
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-03-09 09:09:33 +0000

    update file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,7 +12,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
-int find_strlen(char *s);
+int get_strlen(char *s);
 int check_palindrome(char *s, int len, int index);
 
 #endif
