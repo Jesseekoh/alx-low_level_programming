caf80dfc689229448043d8cc83789e0b8ea399d3
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-03-11 06:22:28 +0000

    add file

---
+++ 0x0A-argc_argv/0-whatismyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - prints name of program
+  * @argc: number of arguments
+  * @argv: all the arguments
+  * Return: 0
+  */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", *argv[0]);
+	return (0);
+}
