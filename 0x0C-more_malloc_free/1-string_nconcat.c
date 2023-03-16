4de3274364ce0b81ec823677c10d2b928db4129a
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-03-17 00:12:21 +0100

    update file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,45 +1,43 @@
 #include "main.h"
 
 /**
-  * string_nconcat - cocatenates two strings
-  * @s1: first string
-  * @s2: second string
-  * @n: number of bytes of s2
-  * Return: pointer to newly allocated space in memory
-  */
+ * string_nconcat - cocatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes of s2
+ * Return: pointer to newly allocated space in memory
+ */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i;
-	char *concat;
-	unsigned int l;
+        int i, len;
+        int j;
+        char *p;
 
-	i = 0;
-	l = 0;
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	while (s1[i])
-	{
-		l++;
-		i++;
-	}
-	concat = malloc(sizeof(char) * (l +n + 1));
-	if (concat == NULL)
-		return (NULL);
-	i = 0;
-	l = 0;
-	while (s1[i])
-	{
-		concat[l++] = s1[i];
-		i++;
-	}
-	i = 0;
-	while (s2[i] && i < n)
-	{
-		concat[l++] = s2[i];
-		i++;
-	}
-	concat[l] = '\0';
-	return (concat);
+        len = 0;
+        i = 0;
+        j = 0;
+
+        if (s1 == NULL)
+                s1 = "";
+        if (s2 == NULL)
+                s2 = "";
+        while (s1[len])
+                len += 1;
+        p = malloc(len + n + 1);
+        if (p == NULL)
+                return (NULL);
+        while (s1[i])
+        {
+                p[i] = s1[i];
+                i += 1;
+        }
+
+        while (j < n)
+        {
+                p[i + j] = s2[j];
+                j += 1;
+        }
+        p[i + j] = '\0';
+        return (p);
 }
