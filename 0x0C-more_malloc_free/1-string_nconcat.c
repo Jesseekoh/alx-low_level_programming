5c94238f1726a8736132ec18be3493949fb27a49
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-03-16 21:26:09 +0000

    update file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,7 +19,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 	while (s1[l])
-		l++
+		l++;
 	newStr = malloc(sizeof(char) * (l + n + 1));
 	if (newStr == NULL)
 		return (NULL);
