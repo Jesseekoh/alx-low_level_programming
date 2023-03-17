afb229745373bbe669e6ce8d276418dae673a0ca
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-03-17 10:04:19 +0000

    add file

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * _calloc - allocates memory for an array using calloc
+  * @nmemb: unsigned int
+  * @size: unsigned int
+  * Return: pointer to allocated memory
+  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	int *arr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	arr = malloc(nmemb * size);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		*(arr + i) = 0;
+	return (arr);
+}
