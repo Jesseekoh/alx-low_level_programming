3d34dac1c21fab81d64a3ba1702e8071027d5a13
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-03-17 15:14:22 +0000

    update file

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,14 +12,16 @@ int *array_range(int min, int max)
 	int *arr;
 	int i, j;
 
+	j = 0;
 	if (min > max)
 		return (NULL);
 	arr = malloc(sizeof(int) * ((max - min) + 1));
 	if (arr == NULL)
 		return (NULL);
-	for (i = 0; i <= max; i++)
+	for (i = min; i <= max; i++)
 	{
-		arr[i] = min + i;
+		arr[j] =  i;
+		j++;
 	}
 	return (arr);
 }
