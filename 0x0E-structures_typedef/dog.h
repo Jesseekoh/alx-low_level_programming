e3abed40ba7bc07f9b142c3ede76d69356bbe8e6
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-03-20 16:12:19 +0000

    update file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,17 +4,18 @@
 #include <stdlib.h>
 
 /**
-  * struct dog - struct
+  * struct dog - dog struct
   * represents a dog
   * @name: name of dog
   * @age: age of dog
   * @owner: owner of dog
   */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
 #endif
