752fa4f1168b290299f6905d042262f8a5cf9b85
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-03-20 16:39:02 +0000

    update file

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)/n");
-		if (d->age > 0)
+		if (d->age >= 0)
 			printf("Age: %f\n", d->age);
 		else
 			printf("Age: (nil)\n");
@@ -22,5 +22,4 @@ void print_dog(struct dog *d)
 		else
 			printf("Owner: (nil)\n");
 	}
-	return;
 }
