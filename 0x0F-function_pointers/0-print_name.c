5143ec20078e0ea058f8fd0ecad09f289d62cb12
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-03-22 15:05:08 +0100

    update files

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,12 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name
- * @namae: pointer of name to be printed
- * @f: pointer to function
- */
-
+  * print_name - prints a name
+  * @name: name to be printed
+  * @f: pointer to function
+  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
-	{
 		f(name);
-	}
 }
