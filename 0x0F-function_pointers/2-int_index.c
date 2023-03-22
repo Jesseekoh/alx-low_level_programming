6410b07e21a76a05959196765355972c2c9c7677
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-03-22 15:54:49 +0100

    add files

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function that searches for an int
+ * @array: array to scan
+ * @size: size of array
+ * @cmp: pointer to function
+ * Return: if no match -1, if match found - index of match
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+        int i;
+
+        if (array == NULL || cmp == NULL)
+                return (-1);
+        if (size <= 0)
+                return (-1);
+        for (i = 0; i < size; i++)
+        {
+                if (cmp(array[i]) != 0)
+                        return (i);
+        }
+        return (-1);
+}
