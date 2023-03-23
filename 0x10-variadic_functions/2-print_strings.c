b8754a036ff73ae1bccc389d79f476c47a58179e
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-03-23 22:54:19 +0000

    update file

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,19 +16,19 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(li, n);
 
-for (i = 0; i < n; i++)
-{
-p = va_arg(args, char *);
-if (p == NULL)
-printf("(nil)");
-else
-printf("%s", p);
+	for (i = 0; i < n; i++)
+	{
+		p = va_arg(args, char *);
+		if (p == NULL)
+			printf("(nil)");
+		else
+			printf("%s", p);
 
-if (i != (n - 1) && separator != NULL)
-printf("%s", separator);
+			if (i != (n - 1) && separator != NULL)
+				printf("%s", separator);
 
-}
+	}
 
-printf("\n");
-va_end(args);
+	printf("\n");
+	va_end(args);
 }
