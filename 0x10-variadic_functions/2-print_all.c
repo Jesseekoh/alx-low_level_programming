56c7d4ae1a4ca8c70edaea33d2bf293d49b8f039
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-03-23 23:15:18 +0000

    add file

---
+++ 0x10-variadic_functions/2-print_all.c
@@ -0,0 +1,45 @@
+#include "variadic_functions.h"
+
+
+void print_all(const char * const format, ...)
+{
+	int i, flag;
+	char *str;
+	va_list argsList;
+
+	va_start(argsList, format);
+	i = 0;
+	while (format != NULL && format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(argsList, int));
+				flag = 0;
+				break;
+			case 'i':
+				printf("%i", va_arg(argsList, int));
+				flag = 0;
+				break;
+			case 'f':
+				printf("%f", va_arg(argsList, double));
+				flag = 0;
+				break;
+			case 's':
+				str = va_arg(argsList, char*);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				flag = 0;
+				break;
+			default:
+				flag = 1;
+				break;
+		}
+		if (*(format + i + 1) != '\0' && flag == 0)
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+	va_end(argsList);
+}
