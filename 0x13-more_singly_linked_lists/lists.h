08d11c0d7baa721eed18e9a969e0cc251bf70c7d
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-04-04 16:43:45 +0100

    add file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -8,9 +8,7 @@
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
- *
  * Description: singly linked list node structure
- *
  */
 typedef struct listint_s
 {
@@ -31,5 +29,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
+listint_t *find_listint_loop(listint_t *head);
 
 #endif
