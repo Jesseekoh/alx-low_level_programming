86edbb4449809b03baa7512b13e728bbe3a4e17a
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-04-10 13:31:47 +0100

    update files and fix errors

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,6 +4,7 @@
  * get_bit - returns the value of a bit at a given index
  * @n: the number
  * @index: index of bit
+ * Return: the value of the bit at the index or -1
  */
 
 int get_bit(unsigned long int n, unsigned int index)
