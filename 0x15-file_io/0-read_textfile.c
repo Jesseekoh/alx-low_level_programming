33aaa1bfc3284f739947aec4bd28d5c44698ddf2
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-04-10 18:30:03 +0100

    update files and fix errors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,7 +10,7 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int o, r, w;
+	size_t o, r, w;
 	char *buffer;
 
 	if (filename == NULL)
@@ -24,13 +24,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	r = read(o, buffer, letters);
 	w = write(1, buffer, r);
 
-	if (o == -1 || w == -1 || r == -1 || w != r)
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
 		return (0);
 	}
 	free(buffer);
-	close(0);
+	close(o);
 
 	return (w);
 }
