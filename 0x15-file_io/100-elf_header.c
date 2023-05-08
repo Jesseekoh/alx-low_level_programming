425e50a44711fc91ad36c4b1bac48c88ac51282e
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-05-08 07:25:02 +0100

    add files

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - displays the information contained in the ELF header at the start of
+ *  an ELF file.
+ * Return: int
+ */
+
+int main(void)
+{
+	return (0);
+}
