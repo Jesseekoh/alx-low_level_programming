214c7aba9b7e7808dc18ebf3cc93eccb830fbabc
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-07-16 05:02:59 +0100

    add 100-elf_header.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,4 +12,15 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
+
+void check_elf(unsigned char *e_ident);
+void print_magic_numbers(unsigned char *e_ident);
+void print_elf_class(unsigned char *e_ident);
+void print_header_data(unsigned char *e_ident);
+void print_elf_version(unsigned char *e_ident);
+void print_elf_abi(unsigned char *e_ident);
+void print_elf_osabi(unsigned char *e_ident);
+void print_elf_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+
 #endif
