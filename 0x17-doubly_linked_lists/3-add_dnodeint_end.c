d1c3949db836d235bd37988790d83fed15dc2a30
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-08-18 00:33:03 +0000

    fix 3-add_dnodent_end.c 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,6 +11,11 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
 
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
 	new_node = malloc(sizeof(dlistint_t));
 
 	if (new_node == NULL)
