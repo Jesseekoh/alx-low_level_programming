a20453636d32ea7ae6416251f3b89883a2520fb9
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-08-18 05:34:05 +0000

    fix file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,5 +10,15 @@
 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *head, *new_node, *prev;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	else if (*h == NULL && idx > 0)
+		return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n 
 
 }
--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -46,4 +46,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		current = current->next;
 		count++;
 	}
+
+	return (-1);
 }
