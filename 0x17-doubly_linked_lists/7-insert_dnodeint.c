9f5546ecdafe3a738c94a672287d9125334ad60d
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-08-18 05:23:34 +0000

    add file

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+
+/**
+ * insert_dnodeint_at_index - insets a value at a give index in a linked list
+ * @h: double head pointer
+ * @idx: index to insert in
+ * @n: value to insert
+ * Return: address of new node or NULL if it fails
+*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+
+}
