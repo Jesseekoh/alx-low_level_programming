054e1825687f834d151a4702ab536f34357a7404
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-08-21 15:39:36 +0000

    task 2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+/**
+ * add - adds two numbers
+ * @a: argument
+ * @b: argument
+ * Return: sum of a and b
+*/
+int add(int a, int b)
+{
+	return (a + b);
+
+}
+
+
+/**
+ * sub - returns the difference between two numbers
+ * @a: argument
+ * @b: argument
+ * Return: int
+*/
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two numbers
+ * @a: int
+ * @b: int
+ * Return: product of the two  numbers
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - division
+ * @a: argument
+ * @b: argument
+ * Return: a ÷ b
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulus
+ * @a: argument
+ * @b: argument
+ * Return: a modulus b
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
