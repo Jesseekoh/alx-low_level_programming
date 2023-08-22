3f3486ea5480885a71fa59b89dd608ae91517afd
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-08-22 20:32:24 +0000

    task 3: fix 2

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/jesseekoh/alx-low_level_programming/main/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp/ https://raw.github.com/Jesseekoh/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD =/tmp/nrandom.so
