8ab10fc752365877091be9a87a8963d1bc5643cc
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-09-13 22:26:06 +0000

    task 1: fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,12 +10,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 	unsigned long int i;
 
-	table = (hash_table_t *) malloc(sizeof(hash_table_t));
+	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 
 	table->size = size;
-	table->array = (hash_node_t **) malloc(sizeof(hash_node_t *) * size);
+	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (!table->array)
 	{
 		free(table);
