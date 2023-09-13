60f723b587ba4721f2a5584dcb683e3d272381c0
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-09-13 19:46:14 +0000

    task 3: fix 2

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -32,6 +32,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 		return (1);
 	}
+	/* Handle collision */
+	if (handle_collision(ht, key, value) == 1)
+		return (1);
+
 	return (0);
 }
 
