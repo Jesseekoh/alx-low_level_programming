8298911daaaf8e3ecfd638dc750d0726a23946f2
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2023-12-12 02:38:52 +0100

    task 0: fix 1

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,13 +15,11 @@ int linear_search(int *array, size_t size, int value)
 
 	for (; i < size; i++)
 	{
+		printf("Value checked array[%lu] = [%i]\n", i, array[i]);
 		if (array[i] == value)
 		{
+
 			return (i);
-		}
-		else
-		{
-			printf("Value checked array[%lu] = [%i]\n", i, array[i]);
 		}
 	}
 
